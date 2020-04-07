13374d81b9ec2360a89108327dfe60bafc110f14
Joseph Omitogun <joseyi2013@gmail.com>
2020-04-07 20:17:33 +0100

    initial update of estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,6 +1,17 @@
 const covid19ImpactEstimator = (data) => {
 
-    
+    const input = data;
+
+    return {
+        input,
+        impact:{
+            currentlyInfected:input.reportedCases*10
+
+        },
+        severeImpact: {
+            currentlyInfected:input.reportedCases*50
+        }
+    }
 };
 
 export default covid19ImpactEstimator;
