ddd7ec5d15ee4ac2b0d3f3e2ec2779611f6aefc9
Joseph Omitogun <joseyi2013@gmail.com>
2020-04-08 20:26:29 +0100

    modified estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,21 +1,22 @@
 const covid19ImpactEstimator = (data) => {
   const input = data;
-
+  const dailyRate1 = input.reportedCases * 10 * 512/28;
+  const dailyRate2 = input.reportedCases * 50 * 512/28;
   return {
     input,
     impact: {
       currentlyInfected: input.reportedCases * 10,
       infectionsByRequestedTime: input.reportedCases * 10 * 512,
-      periodInDays: 12,
-      periodInWeeks: 2,
-      periodInMonths: 1
+      periodInDays: dailyRate1,
+      periodInWeeks: dailyRate1 * 7,
+      periodInMonths: dailyRate1 * 30
     },
     severeImpact: {
       currentlyInfected: input.reportedCases * 50,
       infectionsByRequestedTime: input.reportedCases * 50 * 512,
-      periodInDays: 24,
-      periodInWeeks: 3,
-      periodInMonths: 2
+      periodInDays: dailyRate2,
+      periodInWeeks: dailyRate2 * 7,
+      periodInMonths: dailyRate2 * 30
 
     }
   };
