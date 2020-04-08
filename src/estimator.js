3b4a3a6f655ff00ff5a76f852ec2a1d766701683
Joseph Omitogun <joseyi2013@gmail.com>
2020-04-08 12:16:28 +0100

    modified estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -5,11 +5,11 @@ const covid19ImpactEstimator = (data) => {
     input,
     impact: {
       currentlyInfected: input.reportedCases * 10,
-      infectionsByRequestedTime: input.reportedCases * 10 * 512
+      infectionsByRequestedTime: input.reportedCases * 10 * Math.pow(2,Math.floor(input.timeToElapse/3))
     },
     severeImpact: {
       currentlyInfected: input.reportedCases * 50,
-      infectionsByRequestedTime: input.reportedCases * 50 * 512
+      infectionsByRequestedTime: input.reportedCases * 50 * Math.pow(2,Math.floor(input.timeToElapse/3))
     }
   };
 };
