19bf1d3440e93a173d8f36c29bdc5a89860a8f04
Joseph Omitogun <joseyi2013@gmail.com>
2020-04-09 16:05:41 +0100

    modified estimator.js4

--- src/estimator.js
+++ src/estimator.js
@@ -11,13 +11,13 @@ const covid19ImpactEstimator = (data) => {
 
         if (input.periodType === 'days') {
           const days = input.timeToElapse;
-          ans = 2 ** Math.floor(days / 3) * input.reportedCases * 5120;
+          ans = 2 ** (days / 3) * input.reportedCases * 10;
         } else if (input.periodType === 'weeks') {
           const days = input.timeToElapse * 7;
-          ans = 2 ** Math.floor(days / 3) * input.reportedCases * 5120;
+          ans = 2 ** (days / 3) * input.reportedCases * 10;
         } else {
           const days = input.timeToElapse * 30;
-          ans = 2 ** Math.floor(days / 3) * input.reportedCases * 5120;
+          ans = 2 ** (days / 3) * input.reportedCases * 10;
         }
         return ans;
       }
@@ -29,13 +29,13 @@ const covid19ImpactEstimator = (data) => {
         let ans = 0;
         if (input.periodType === 'days') {
           const days = input.timeToElapse;
-          ans = 2 ** Math.floor(days / 3) * input.reportedCases * 50;
+          ans = 2 ** (days / 3) * input.reportedCases * 50;
         } else if (input.periodType === 'weeks') {
           const days = input.timeToElapse * 7;
-          ans = 2 ** Math.floor(days / 3) * input.reportedCases * 50;
+          ans = 2 ** (days / 3) * input.reportedCases * 50;
         } else {
           const days = input.timeToElapse * 30;
-          ans = 2 ** Math.floor(days / 3) * input.reportedCases * 50;
+          ans = 2 ** (days / 3) * input.reportedCases * 50;
         }
         return ans;
       }
