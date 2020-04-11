ba244fb670aa64488354c6b3badd7d8df2f4b8f5
Joseph Omitogun <joseyi2013@gmail.com>
2020-04-11 20:40:30 +0100

    modified estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -6,7 +6,7 @@ const covid19ImpactEstimator = (data) => {
     impact: {
       currentlyInfected: input.reportedCases * 10,
       infectionsByRequestedTime: input.reportedCases * 10 * 512,
-      /* estimator() {
+       estimator() {
         let ans = 0;
 
         if (input.periodType === 'days') {
@@ -20,23 +20,13 @@ const covid19ImpactEstimator = (data) => {
           ans = 2 ** (days / 3) * input.reportedCases * 10;
         }
         return ans;
-      } */
-      estimator() {
-        let ans;
-        if (input.periodType === 'days') {
-          ans = 12142510080;
-        } else if (input.periodType === 'weeks') {
-          ans = 1021050880;
-        } else {
-          ans = 6904159928320;
-        }
-        return ans;
-      }
+      } 
+      
     },
     severeImpact: {
       currentlyInfected: input.reportedCases * 50,
       infectionsByRequestedTime: input.reportedCases * 50 * 512,
-      /* estimator() {
+       estimator() {
         let ans = 0;
         if (input.periodType === 'days') {
           const days = input.timeToElapse;
@@ -49,18 +39,8 @@ const covid19ImpactEstimator = (data) => {
           ans = 2 ** Math.floor(days / 3) * input.reportedCases * 50;
         }
         return ans;
-      } */
-      estimator() {
-        let ans;
-        if (input.periodType === 'days') {
-          ans = 1080;
-        } else if (input.periodType === 'weeks') {
-          ans = 2281701376000;
-        } else {
-          ans = 5284823040;
-        }
-        return ans;
-      }
+      } 
+      
     }
   };
 };
