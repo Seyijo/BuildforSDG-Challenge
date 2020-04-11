0ae66c8f7002cf048a0b4efbc563f73841073117
Joseph Omitogun <joseyi2013@gmail.com>
2020-04-11 21:29:50 +0100

    modified estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -6,38 +6,47 @@ const covid19ImpactEstimator = (data) => {
     impact: {
       currentlyInfected: input.reportedCases * 10,
       infectionsByRequestedTime: input.reportedCases * 10 * 512,
-      estimator() {
+      periodInDays() {
         let ans = 0;
-
-        if (input.periodType === 'days') {
-          const days = input.timeToElapse;
+        const days = input.timeToElapse;
           ans = 2 ** (days / 3) * input.reportedCases * 10;
-        } else if (input.periodType === 'weeks') {
+          return ans;
+      },
+        periodInWeeks(){
+          let ans = 0;
           const days = input.timeToElapse * 7;
-          ans = 2 ** (days / 3) * input.reportedCases * 10;
-        } else {
-          const days = input.timeToElapse * 30;
-          ans = 2 ** (days / 3) * input.reportedCases * 10;
-        }
+          ans = 2 ** Math.floor(days / 3) * input.reportedCases * 10;
         return ans;
-      }
+        },
+        periodInMonths(){
+          let ans = 0;
+          const days = input.timeToElapse * 30;
+          ans = 2 ** Math.floor(days / 3) * input.reportedCases * 10;
+          return ans;
+        }
     },
     severeImpact: {
       currentlyInfected: input.reportedCases * 50,
       infectionsByRequestedTime: input.reportedCases * 50 * 512,
-      estimator() {
+      periodInDays() {
         let ans = 0;
-        if (input.periodType === 'days') {
-          const days = input.timeToElapse;
+        const days = input.timeToElapse;
           ans = 2 ** (days / 3) * input.reportedCases * 50;
-        } else if (input.periodType === 'weeks') {
+          return ans;
+      },
+        periodInWeeks(){
+          let ans = 0;
           const days = input.timeToElapse * 7;
           ans = 2 ** Math.floor(days / 3) * input.reportedCases * 50;
-        } else {
+        return ans;
+        },
+        periodInMonths(){
+          let ans = 0;
           const days = input.timeToElapse * 30;
           ans = 2 ** Math.floor(days / 3) * input.reportedCases * 50;
+          return ans;
         }
-        return ans;
+        
       }
     }
   };
